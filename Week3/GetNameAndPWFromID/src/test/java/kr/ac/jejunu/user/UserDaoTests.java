59b7628b062ae68ad1d30acd90b9608ccec2ba6b
alzseven <jhp7140@gmail.com>
2021-04-30 17:02:00 +0900

    Remove non-usings

--- Week3/GetNameAndPWFromID/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
+++ Week3/GetNameAndPWFromID/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
@@ -97,38 +97,5 @@ public class UserDaoTests {
 
         assertThat(deletedUser, nullValue());
     }
-
-//    @Test
-//    public void getHalla() throws SQLException, ClassNotFoundException {
-//        Integer id = 1;
-//        String name = "HallaJHP";
-//        String password = "HallaJHPPW";
-//
-//        UserDao userDao = new UserDao(new HallaConnectionMaker());
-//        User user = userDao.findById(id);
-//        assertThat(user.getId(), is(id));
-//        assertThat(user.getName(), is(name));
-//        assertThat(user.getPassword(), is(password));
-//    }
-//
-//
-//    @Test
-//    public void insertHalla() throws SQLException, ClassNotFoundException{
-//        String name = "JHP";
-//        String password = "1111";
-//
-//        User user = new User();
-//        user.setName(name);
-//        user.setPassword(password);
-//
-//        UserDao userDao = new UserDao(new HallaConnectionMaker());
-//        userDao.insert(user);
-//
-//        User insertedUser = userDao.findById(user.getId());
-//
-//        assertThat(insertedUser.getId(), greaterThan(0));
-//        assertThat(insertedUser.getId(), is(user.getId()));
-//        assertThat(insertedUser.getName(), is(user.getName()));
-//        assertThat(insertedUser.getPassword(), is(user.getPassword()));
-//    }
+    
 }
